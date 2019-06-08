15d6cb8b252ccd6e775a664a98aead7d1fd2cbb3
HectorGabucio <hectordorito5@gmail.com>
2019-06-08 13:18:40 +0200

    tslint

--- cajamar-scraper.ts
+++ cajamar-scraper.ts
@@ -1,148 +1,157 @@
-"use strict";
+'use strict'
 
-import crypto from "crypto";
-import puppeteer from "puppeteer";
+import crypto from 'crypto'
+import puppeteer from 'puppeteer'
 
-const separator = "|||";
+const separator = '|||'
 
 interface IMovement {
-    id: string;
-    date: string;
-    concept: string;
-    amount: string;
+    id: string
+    date: string
+    concept: string
+    amount: string
 }
 
 function scrapLine(line: string) {
-
-    const parts: string[] = line.split(separator);
+    const parts: string[] = line.split(separator)
     if (parts.length !== 3) {
-        return {};
+        return {}
     }
 
     const result: IMovement = {
-        id: crypto.createHash("sha256").update(JSON.stringify(parts)).digest("hex"),
+        id: crypto
+            .createHash('sha256')
+            .update(JSON.stringify(parts))
+            .digest('hex'),
         date: parts[0],
         concept: parts[1],
         amount: parts[2],
-    };
-    return result;
+    }
+    return result
 }
 
-function waitForFrame(page: puppeteer.Page, frameName: string): Promise<puppeteer.Frame> {
-    let fulfill;
-    const promise: Promise<puppeteer.Frame> = new Promise((x) => fulfill = x);
-    checkFrame();
-    return promise;
+function waitForFrame(
+    page: puppeteer.Page,
+    frameName: string
+): Promise<puppeteer.Frame> {
+    let fulfill
+    const promise: Promise<puppeteer.Frame> = new Promise(x => (fulfill = x))
+    checkFrame()
+    return promise
 
     function checkFrame() {
-        const frame = page.frames().find((f) => f.name() === frameName);
+        const frame = page.frames().find(f => f.name() === frameName)
         if (frame) {
-            fulfill(frame);
+            fulfill(frame)
         } else {
-            page.once("frameattached", checkFrame);
+            page.once('frameattached', checkFrame)
         }
     }
 }
 
 export async function getResult() {
-    let options = {};
+    let options = {}
 
-    if (process.env.ENVIRONMENT && process.env.ENVIRONMENT === "local") {
+    if (process.env.ENVIRONMENT && process.env.ENVIRONMENT === 'local') {
         options = {
-            headless: false, args: [
-                "--start-maximized", // you can also use '--start-fullscreen'
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-                "--proxy-server='direct://'", "--proxy-bypass-list=*",
-            ], slowMo: 150,
-        };
+            headless: false,
+            args: [
+                '--start-maximized', // you can also use '--start-fullscreen'
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                "--proxy-server='direct://'",
+                '--proxy-bypass-list=*',
+            ],
+            slowMo: 150,
+        }
     } else {
         options = {
             args: [
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-                "--proxy-server='direct://'", "--proxy-bypass-list=*",
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                "--proxy-server='direct://'",
+                '--proxy-bypass-list=*',
             ],
-        };
+        }
     }
 
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
+    const browser = await puppeteer.launch(options)
+    const page = await browser.newPage()
 
-    await page.setRequestInterception(true);
-    page.on("request", (req) => {
-        if (req.resourceType() == "font" || req.resourceType() == "image") {
-            req.abort();
+    await page.setRequestInterception(true)
+    page.on('request', req => {
+        if (req.resourceType() == 'font' || req.resourceType() == 'image') {
+            req.abort()
         } else {
-            req.continue();
+            req.continue()
         }
-    });
+    })
 
-    await page.goto("https://www.cajamar.es/es/comun/acceder-a-banca-electronica-reintentar/");
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.type("#COD_NEW3", process.env.USER);
-    await page.type("#PASS_NEW3", process.env.PASS);
-    await page.click(".lnkAceptar");
-    await page.waitFor("#principaln1_cuentas");
-    await page.click("#principaln1_cuentas");
-    await page.click("a[data-id=n3_movimientos]");
-    page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+    await page.goto(
+        'https://www.cajamar.es/es/comun/acceder-a-banca-electronica-reintentar/'
+    )
+    await page.setViewport({ width: 1920, height: 1080 })
+    await page.type('#COD_NEW3', process.env.USER)
+    await page.type('#PASS_NEW3', process.env.PASS)
+    await page.click('.lnkAceptar')
+    await page.waitFor('#principaln1_cuentas')
+    await page.click('#principaln1_cuentas')
+    await page.click('a[data-id=n3_movimientos]')
+    page.on('console', msg => console.log('PAGE LOG:', msg.text()))
 
-    const frame = await waitForFrame(page, "contenido");
-    await frame.waitForSelector("button");
+    const frame = await waitForFrame(page, 'contenido')
+    await frame.waitForSelector('button')
 
     await frame.evaluate(() => {
         // document.querySelector(".z-spinner-input").textContent = "0"
-        document.querySelector<HTMLInputElement>(".z-spinner-input").value = "0";
+        document.querySelector<HTMLInputElement>('.z-spinner-input').value = '0'
+    })
 
-    });
+    await frame.type('.z-spinner-input', '180')
 
-    await frame.type(".z-spinner-input", "180");
+    await frame.focus('button')
 
-    await frame.focus("button");
+    await frame.waitFor(1000)
 
-    await frame.waitFor(1000);
+    await frame.click('button')
 
-    await frame.click("button");
+    const frame2 = await waitForFrame(page, 'contenido')
+    await frame2.waitForSelector('.z-column-content')
 
-    const frame2 = await waitForFrame(page, "contenido");
-    await frame2.waitForSelector(".z-column-content");
-
-    await frame2.click(".z-column-content");
-    await frame2.waitFor(".z-icon-caret-up");
-    await frame2.click(".z-column-content");
-    await frame2.waitFor(".z-icon-caret-down");
+    await frame2.click('.z-column-content')
+    await frame2.waitFor('.z-icon-caret-up')
+    await frame2.click('.z-column-content')
+    await frame2.waitFor('.z-icon-caret-down')
 
     const movements = await frame2.evaluate(() => {
-
-        let result = "";
-        const numberCols = 3;
+        let result = ''
+        const numberCols = 3
         const elements = Array.from(
             document.querySelectorAll(
-                ".z-cell[data-title='Concepto'],.z-cell[data-title='Fecha'],.z-cell[data-title='Importe']"));
+                ".z-cell[data-title='Concepto'],.z-cell[data-title='Fecha'],.z-cell[data-title='Importe']"
+            )
+        )
 
         elements.forEach((element, index) => {
-            const i = index + 1;
+            const i = index + 1
 
             // console.log("INDEX " + i, "ELEMENT " + element.innerText + " MODULUS " + (i%(numberCols)))
 
-            if (i % (numberCols) === 0) {
-                result += element.textContent + "\n";
+            if (i % numberCols === 0) {
+                result += element.textContent + '\n'
             } else {
-                result += element.textContent + "|||";
+                result += element.textContent + '|||'
             }
-        });
+        })
 
-        return result;
+        return result
+    })
 
-    });
+    await browser.close()
 
-    await browser.close();
+    const resFinal = movements.split('\n').map(x => {
+        return scrapLine(x)
+    })
 
-    const resFinal = movements.split("\n").map((x) => {
-        return scrapLine(x);
-    });
-
-    return resFinal;
-
+    return resFinal
 }
--- main.ts
+++ main.ts
@@ -1,66 +1,71 @@
-"use strict";
+'use strict'
 
-import dotenv from "dotenv";
-import express from "express";
-import basicAuth from "express-basic-auth";
-const app = express();
+import dotenv from 'dotenv'
+import express from 'express'
+import basicAuth from 'express-basic-auth'
+const app = express()
 
 // const cajamar = require("./cajamar-scraper");
-import * as cajamar from "./cajamar-scraper";
-const port = process.env.PORT || 3000;
+import * as cajamar from './cajamar-scraper'
+const port = process.env.PORT || 3000
 
-loadEnv();
+loadEnv()
 
 function getUnauthorizedResponse(req) {
-    return req.auth
-        ? ("Credentials rejected")
-        : "No credentials provided";
+    return req.auth ? 'Credentials rejected' : 'No credentials provided'
 }
 
-app.use(basicAuth({
-    users: {
-        user: process.env.BASIC_PASS,
-    },
-    unauthorizedResponse: getUnauthorizedResponse,
-}));
+app.use(
+    basicAuth({
+        users: {
+            user: process.env.BASIC_PASS,
+        },
+        unauthorizedResponse: getUnauthorizedResponse,
+    })
+)
 
 function errorHandler(err, req, res, next) {
-    console.log(console.log(err));
-    res.status(500).send({ success: false });
+    console.log(console.log(err))
+    res.status(500).send({ success: false })
 }
 
-const wrapAsync = (fn) => {
+const wrapAsync = fn => {
     return (req, res, next) => {
-        const fnReturn = fn(req, res, next);
-        return Promise.resolve(fnReturn).catch(next);
-    };
-};
-
-app.get("/", wrapAsync(async function example(req, res) {
-
-    const hrstart = process.hrtime();
-    const result = await cajamar.getResult();
-    const hrend = process.hrtime(hrstart);
-
-    console.info("Execution time (hr): %ds %dms", hrend[0], hrend[1] / 1000000);
-
-    res.status(200).send(result);
-}));
-
-app.listen(port, () => console.log("Example app listening on port " + port));
-
-process.on("unhandledRejection", (reason, promise) => {
-    console.log("Unhandled Rejection at:", reason);
-    process.exit(1);
-});
-
-function loadEnv() {
-
-    const result = dotenv.config();
-    if (result.error) {
-        // throw result.error
-        console.log("no env file, using config vars");
+        const fnReturn = fn(req, res, next)
+        return Promise.resolve(fnReturn).catch(next)
     }
 }
 
-app.use(errorHandler);
+app.get(
+    '/',
+    wrapAsync(async function example(req, res) {
+        const hrstart = process.hrtime()
+        const result = await cajamar.getResult()
+        const hrend = process.hrtime(hrstart)
+
+        console.info(
+            'Execution time (hr): %ds %dms',
+            hrend[0],
+            hrend[1] / 1000000
+        )
+
+        res.status(200).send(result)
+    })
+)
+
+app.listen(port, () => console.log('Example app listening on port ' + port))
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled Rejection at:', reason)
+    process.exit(1)
+})
+
+function loadEnv() {
+    const result = dotenv.config()
+    if (result.error) {
+        // throw result.error
+        console.log('no env file, using config vars')
+    }
+}
+
+app.use(errorHandler)
